3d5393c8784ddfe50890a3294a2e68235b00d2a7
ducbinh02 <ducbinhnguyen.2002@gmail.com>
2023-10-24 20:48:53 +0700

    create new creat and get all api for dishcategory

---
+++ Application/Features/DishCategorys/Commands/DishCategoryCreateEvent.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DishCategorys.Commands
+{
+
+    public class DishCategoryCreateEvent : BaseEvent
+    {
+        public DishCategory DishCategory { get; }
+
+
+        public DishCategoryCreateEvent(DishCategory DishCategory)
+        {
+            DishCategory = DishCategory;
+        }
+    }
+}
---
+++ Application/Features/DishCategorys/Queries/GetAllDishCategoriesDto.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Mappings;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DishCategorys.Queries
+{
+   
+    public class GetAllDishCategoriesDto : IMapFrom<DishCategory>
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public byte isActived { get; set; }
+
+    }
+}
