97eebed157451f53557330476edb5d20ee5676db
ducbinh02 <ducbinhnguyen.2002@gmail.com>
2023-10-24 15:50:57 +0700

    create new create, get all api for certification

---
+++ Application/Features/Certifications/Queries/GetAllCertificationsDto.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Mappings;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Certifications.Queries
+{
+    public class GetAllCertificationsDto : IMapFrom<Certification>
+    {
+        public string Name { get; set; }
+        public Guid ChefID { get; set; }
+        public string IssuingAuthority { get; set; }
+        public string CertificationDescription { get; set; }
+        public string EffectiveDate { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CertificationURL { get; set; }
+        public byte IsValid { get; set; }
+        
+    }
+}
---
+++ OnlyFoodApp/Controllers/CertificationsController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.Campaigns.Commands.CreateCampaign;
+using Application.Features.Campaigns.Queries.GetAllCampaigns;
+using Application.Features.Certifications.Commands;
+using Application.Features.Certifications.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace OnlyFoodApp.Controllers
+{
+    public class CertificationsController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CertificationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Result<List<GetAllCertificationsDto>>>> GetAll()
+        {
+            return await _mediator.Send(new GetAllCertificationsQuery());
+        }
+
+        [HttpPost]
+        public async Task<Result<Guid>> CreateCampaigns(CreateCertificationCommand certification)
+        {
+            var a = certification;
+            return await _mediator.Send(certification);
+        }
+    }
+}
