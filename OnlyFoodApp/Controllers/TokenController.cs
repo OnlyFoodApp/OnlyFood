608e37e9f2f0a37b3adf106250845f7ae5a55feb
fatnotfat <nguyenphat2711@gmail.com>
2023-11-11 06:47:36 +0700

    fix add chef id to login api

--- OnlyFoodApp/Controllers/TokenController.cs
+++ OnlyFoodApp/Controllers/TokenController.cs
@@ -2,12 +2,15 @@
 using System.Security.Claims;
 using System.Text;
 using Application.Features.Accounts.Queries.GetAccountWithEmailAndPassword;
+using Application.Interfaces.Repositories;
+using Azure.Core;
 using Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Persistence.Contexts;
+using Persistence.Repositories;
 
 namespace OnlyFoodApp.Controllers
 {
@@ -18,11 +21,13 @@ namespace OnlyFoodApp.Controllers
     {
         public IConfiguration _configuration;
         private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public TokenController(IConfiguration config, ApplicationDbContext context)
+        public TokenController(IConfiguration config, ApplicationDbContext context, IUnitOfWork unitOfWork)
         {
             _configuration = config;
             _context = context;
+            _unitOfWork = unitOfWork;
         }
 
         [HttpPost]
@@ -49,8 +54,36 @@ namespace OnlyFoodApp.Controllers
                         role = "Customer";
                     }
 
-                    //create claims details based on the user information
-                    var claims = new[] {
+                    var chef = _unitOfWork.Repository<Chef>().Entities.Where(a => a.AccountId.Equals(user.Id)).SingleOrDefault();
+                    if (chef != null)
+                    {
+                        var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        //new Claim("Roles", role),
+                        new Claim(ClaimTypes.Role, role),
+                        new Claim("Id", user.Id.ToString()),
+                        new Claim("LastName", user.LastName),
+                        new Claim("UserName", user.Username),
+                        new Claim("Email", user.Email),
+                        new Claim("ChefId", chef.Id.ToString()),
+                        new Claim("Role", role)
+                    };
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                        var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                            _configuration["Jwt:Issuer"],
+                            _configuration["Jwt:Audience"],
+                            claims,
+                            expires: DateTime.UtcNow.AddMinutes(60),
+                            signingCredentials: signIn);
+
+                        return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                    }
+                    else
+                    {
+                        var claims = new[] {
                         new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
                         new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                         new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
@@ -63,16 +96,20 @@ namespace OnlyFoodApp.Controllers
                         new Claim("Role", role)
                     };
 
-                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-                    var token = new JwtSecurityToken(
-                        _configuration["Jwt:Issuer"],
-                        _configuration["Jwt:Audience"],
-                        claims,
-                        expires: DateTime.UtcNow.AddMinutes(60),
-                        signingCredentials: signIn);
+                        //create claims details based on the user information
 
-                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                        var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                            _configuration["Jwt:Issuer"],
+                            _configuration["Jwt:Audience"],
+                            claims,
+                            expires: DateTime.UtcNow.AddMinutes(60),
+                            signingCredentials: signIn);
+
+                        return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                    }
+                   
                 }
                 else
                 {
@@ -88,10 +125,10 @@ namespace OnlyFoodApp.Controllers
 
         private async Task<Account> GetUser(string email, string password)
         {
-            return await _context.Accounts.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+            return await _context.Accounts.Include(c => c.Chef).FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
         }
 
-        
+
     }
 }
 
